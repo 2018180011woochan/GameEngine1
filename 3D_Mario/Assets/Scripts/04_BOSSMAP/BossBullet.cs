789c7d7491fd95595afe11ebb381a6aabd559c68
DESKTOP-452275K\wc542 <wc5427@naver.com>
2022-05-16 05:09:22 +0900

    보스 행동패턴 FSM으로 변경, 애니메이션 수정

---
+++ 3D_Mario/Assets/Scripts/04_BOSSMAP/BossBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    public int damage;
+    public bool isMelee;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject, 3);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isMelee && other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 3D_Mario/Assets/Scripts/04_BOSSMAP/BossEnemy.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossEnemy : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+    public Transform target;
+    public BoxCollider meleeArea;
+    public bool isChase;
+    public bool isAttack;
+    
+    private Rigidbody rigid;
+    private BoxCollider boxCollider;
+    //private MeshRenderer meshs;
+    private NavMeshAgent nav;
+    private Animator anim;
+
+    private Vector3 lookVec;
+    private Vector3 tauntVec;
+    public bool isLook;
+    
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        //meshs = GetComponentInChildren<MeshRenderer>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        Invoke("ChaseStart", 0.1f);
+        StartCoroutine(Think());
+        isLook = true;
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+
+    private void Update()
+    {
+        if (isLook)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            lookVec = new Vector3(h, 0, v) * 5f;
+        }
+
+        // if (anim.GetBool("isWalk"))
+        // {
+        //     isChase = true;
+        // }
+        // else
+        // {
+        //     isChase = false;
+        // }
+        //
+       if (nav.enabled)
+       {
+           nav.SetDestination(target.position);
+           nav.isStopped = !isChase;
+       }
+    }
+
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void Targerting()
+    {
+        float targetRadius = 1.5f;
+        float targetRange = 3f;
+        
+        RaycastHit[] rayHits =
+            Physics.SphereCastAll(transform.position,
+                targetRadius,
+                transform.forward, 
+                targetRange,
+                LayerMask.GetMask("Player"));
+
+        if (rayHits.Length > 0 && !isAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        isChase = false;
+        isAttack = true;
+        anim.SetBool("isAttack", true);
+        
+        yield return new WaitForSeconds(0.2f);
+        meleeArea.enabled = true;
+
+        yield return new WaitForSeconds(1f);
+        meleeArea.enabled = false;
+
+        
+        yield return new WaitForSeconds(0.2f);
+        meleeArea.enabled = true;
+        
+        yield return new WaitForSeconds(1f);
+        meleeArea.enabled = false;
+
+        isChase = true;
+        isAttack = false;
+        anim.SetBool("isAttack", false);
+    }
+    
+    
+    IEnumerator Think()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        int ranAction = UnityEngine.Random.Range(0, 5);
+
+        switch (ranAction)
+        {
+            case 0:
+            case 1:
+                // 플레이어 쫓아가는 패턴
+                //StartCoroutine(ChasePlayer());
+                //break;
+            case 2:
+                // 점프공격 패턴
+                StartCoroutine(Taunt());
+                break;
+            case 3:
+                // 돌굴러가는 패턴
+                StartCoroutine(RockShot());
+                break;
+            case 4:
+                // 미사일쏘는 패턴
+                StartCoroutine(FireMissile());
+                break;
+        }
+    }
+
+    IEnumerator ChasePlayer()
+    {
+        Debug.Log("chase player");
+        //Invoke("ChaseStart", 2);
+        
+        isChase = true;
+        anim.SetBool("isWalk", true);
+        
+        if (nav.enabled)
+        {
+            nav.SetDestination(target.position);
+            nav.isStopped = !isChase;
+        }
+        
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Think());
+    }
+    
+    IEnumerator RockShot()
+    {
+        Debug.Log("rockshot");
+
+        anim.SetTrigger("doRock");
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Think());
+    }
+    
+    IEnumerator Taunt()
+    {
+        Debug.Log("jump");
+
+        anim.SetTrigger("doJump");
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Think());
+    }
+    
+    IEnumerator FireMissile()
+    {
+        Debug.Log("FireMissile");
+
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Think());
+    }
+    
+    void FixedUpdate()
+    {
+        Targerting();
+        FreezeVelocity();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Melee")
+        {
+            curHealth -= 10;
+            Vector3 reactVec = transform.position - other.transform.position;
+            StartCoroutine(OnDamage(reactVec));
+        }
+    }
+
+    IEnumerator OnDamage(Vector3 reactVec)
+    {
+
+        yield return new WaitForSeconds(0.1f);
+
+        reactVec = reactVec.normalized;
+        reactVec += Vector3.up;
+        rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+        
+        if (curHealth > 0)
+        {
+            //mat.color = Color.white;
+        }
+    }
+
+}
