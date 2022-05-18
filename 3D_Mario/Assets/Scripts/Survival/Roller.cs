a279083a8e4b755078b01abb5173cc58bf6f4780
mi1128h <mihyeon001128@gmail.com>
2022-05-18 21:49:00 +0900

    Spike Roller 이동

---
+++ 3D_Mario/Assets/Scripts/Survival/Roller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roller : MonoBehaviour
+{
+	private bool collide;
+	private Vector3 direction;
+    
+    void Start()
+    {
+		collide = false;
+		direction = new Vector3(0, 0, 1);
+    }
+
+    void FixedUpdate()
+    {
+		transform.Translate(direction * Time.deltaTime, Space.World);
+    }
+
+	void OnCollisionEnter(Collision collision)
+    {
+		if(!collide)
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+     		direction.z *= -1;
+			collide = true;
+        }
+    }
+
+	void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Brick"))
+        {
+			collide = false;
+        }
+	}
+}
