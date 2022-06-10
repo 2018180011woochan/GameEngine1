f4d9e1f922691b725bbf1d6f327fffbcd52d9d8b
dirma10 <syys0104@gmail.com>
2022-06-11 01:24:24 +0900

    Grrrol 만드는중

---
+++ 3D_Mario/Assets/Scripts/COMMON_STAGE/Grrrol.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grrrol : MonoBehaviour
+{
+    Transform trans;
+    float speed = 0.06f;
+    public bool isRight;
+
+    void Awake()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        // -7 ~ 7
+        if (isRight)
+        {
+            trans.Translate(Vector3.right * speed);
+            if (trans.position.x >= 7f) isRight = false;
+        }
+
+        else
+        {
+            trans.Translate(Vector3.left * speed);
+            if (trans.position.x <= -7f) isRight = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.tag == "Player")
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+}
